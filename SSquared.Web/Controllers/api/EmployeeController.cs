60e6977dcfe6b82d798ab868bda068b166a8d679
mkmebarkia <mkmebarkia@gmail.com>
2022-04-25 02:54:36 -0500

    fix api

--- SSquared.Web/Controllers/api/EmployeeController.cs
+++ SSquared.Web/Controllers/api/EmployeeController.cs
@@ -4,7 +4,7 @@ using SSquared.Core.Models;
 using SSquared.Core.Services.Interfaces;
 using SSquared.Web.Models;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 
 namespace SSquared.Web.Controllers.api
 {
